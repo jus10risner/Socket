052783517f892fc46e46e336a74e406b04bd9be5
Justin <justinrisner@gmail.com>
2024-05-07 12:52:26 -0400

    [Clean] Added documentation, to explain the ConditionalListRowSpacing modifier

--- SocketCD/View Modifiers/ConditionalListRowSpacing.swift
+++ SocketCD/View Modifiers/ConditionalListRowSpacing.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+// iOS 15 doesn't round the corners of each list item when spacing is applied, which looks unpolished. This modifier adds list row spacing only to iOS 16+.
 struct ConditionalListRowSpacing: ViewModifier {
     func body(content: Content) -> some View {
         if #available(iOS 16, *) {
