95e1880b974311464a4d9591dd92e73957460124
Justin <justinrisner@gmail.com>
2024-05-30 10:21:20 -0400

    [Feature] Redesigned FuelEconomyInfoView to look like FillTypeInfoView, for consistency

--- SocketCD/Fill-ups/Fill-ups Components/FuelEconomyInfoView.swift
+++ SocketCD/Fill-ups/Fill-ups Components/FuelEconomyInfoView.swift
@@ -19,50 +19,55 @@ struct FuelEconomyInfoView: View {
     
     private var fuelEconomyInfo: some View {
         AppropriateNavigationType {
-            ScrollView {
-                VStack(alignment: .leading) {
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Why no fuel economy?")
-                            .font(.title2.bold())
-                            .foregroundStyle(Color.selectedColor(for: .fillupsTheme))
+            Form {
+                Section {
+                    VStack(alignment: .leading) {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Fuel economy is only calculated between consecutive **Full Tank** fill-ups. It looks like one of your most recent fill-ups was a **Partial Fill** or **Missed Fill-up**.")
+                            
+                            Text("See below, to learn about fuel economy calculation for different fill types.")
+                        }
+                        .padding()
                         
-                        Text("Fuel economy is only calculated between consecutive **Full Tank** fill-ups. One of your most recent fill-ups was a **Partial Fill** or **Missed Fill-up**.")
+                        Divider()
                         
-                        Text("See below, to learn about fuel economy calculation for different fill types.")
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Partial Fill")
+                                .font(.headline)
+                                .foregroundStyle(Color.selectedColor(for: .fillupsTheme))
+                            
+                            Text("This fill type is used only to estimate average fuel economy for this vehicle.")
+                            
+                            Text("Fuel economy calculation will resume after two consecutive Full Tank fill-ups have been added.")
+                        }
+                        .padding()
+                        
+                        Divider()
+                        
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("Missed Fill-up")
+                                .font(.headline)
+                                .foregroundStyle(Color.selectedColor(for: .fillupsTheme))
+                            
+                            Text("This fill type is treated like your very first fill-up, and fuel economy is not calculated.")
+                            
+                            Text("Fuel economy calculation will resume after one more Full Tank fill-up has been added.")
+                            
+                            Text("**Note:** This fill type should only be selected when your fuel tank is full. If the fuel tank was not full when this fill-up was added, your fuel economy may be inaccurate when it is calculated again.")
+                                .font(.caption)
+                                .foregroundStyle(Color.secondary)
+                        }
+                        .padding()
                     }
-                    .padding()
-                    
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Partial Fill")
-                            .font(.headline)
-                            .foregroundStyle(Color.selectedColor(for: .fillupsTheme))
-                        
-                        Text("This fill type is used only to estimate average fuel economy for this vehicle.")
-                        
-                        Text("Fuel economy calculation will resume after two consecutive Full Tank fill-ups have been added.")
-                    }
-                    .padding()
-                    
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Missed Fill-up")
-                            .font(.headline)
-                            .foregroundStyle(Color.selectedColor(for: .fillupsTheme))
-                        
-                        Text("This fill type is treated like your very first fill-up, and fuel economy is not calculated.")
-                        
-                        Text("Fuel economy calculation will resume after one more Full Tank fill-up has been added.")
-                        
-                        Text("**Note:** This fill type should only be selected when your fuel tank is full. If the fuel tank was not full when this fill-up was added, your fuel economy may be inaccurate when it is calculated again.")
-                            .font(.caption)
-                            .foregroundStyle(Color.secondary)
-                    }
-                    .padding()
+                }  header: {
+                    Text("Why no fuel economy?")
+                        .font(.headline)
+                        .frame(maxWidth: .infinity)
                 }
+                .headerProminence(.increased)
             }
             .font(.subheadline)
-            .navigationTitle("")
             .navigationBarTitleDisplayMode(.inline)
-            .padding(20)
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button {
