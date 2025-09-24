7f8de12ecb225406b22f880ffcf3a1fc648c2d44
Justin <justinrisner@gmail.com>
2025-09-24 15:29:29 -0400

    [Feature] Updated the view and section footer text

--- SocketCD/Settings/MaintenanceAlertSettingsView.swift
+++ SocketCD/Settings/MaintenanceAlertSettingsView.swift
@@ -10,64 +10,30 @@ import SwiftUI
 struct MaintenanceAlertSettingsView: View {
     @EnvironmentObject var settings: AppSettings
     
-    @FocusState var isInputActive: Bool
-    
     var body: some View {
         List {
-            Section(footer: Text("Specifies how far in advance Socket should alert you of upcoming maintenance services.")) {
-                distanceBeforeDueField
+            Section(footer: Text("When Socket should alert you of upcoming maintenance services")) {
+                LabeledInput(label: "\(settings.distanceUnit.rawValue.capitalized) before due") {
+                    TextField("\(settings.distanceUnit.rawValue.capitalized)", value: $settings.distanceBeforeMaintenance, format: .number.decimalSeparator(strategy: .automatic))
+                        .keyboardType(.numberPad)
+                }
                 
-                daysBeforeDueField
+                LabeledInput(label: "Days before due") {
+                    TextField("Days", value: $settings.daysBeforeMaintenance, format: .number.decimalSeparator(strategy: .automatic))
+                        .keyboardType(.numberPad)
+                }
             }
             
-            Section(footer: Text("Navigates to the Settings app, where you can make changes to Socket's notification preferences.")) {
-                socketSettingsButton
+            Section(footer: Text("Manage Socket’s notification preferences in the Settings app.")) {
+                Button("Notification Settings") {
+                    Task {
+                        await AppSettings.openSocketSettings()
+                    }
+                }
             }
         }
         .navigationTitle("Alerts")
         .navigationBarTitleDisplayMode(.inline)
-    }
-    
-    
-    // MARK: - Views
-    
-    // TextField to specify how many mi/km before maintenance is due that the user would like to be notified
-    private var distanceBeforeDueField: some View {
-        HStack {
-            Text("\(settings.distanceUnit.rawValue.capitalized) before due")
-            
-            Spacer()
-            
-            TextField("\(settings.distanceUnit.rawValue.capitalized)", value: $settings.distanceBeforeMaintenance, format: .number.decimalSeparator(strategy: .automatic))
-                .keyboardType(.numberPad)
-                .fixedSize()
-                .textFieldStyle(.roundedBorder)
-                .focused($isInputActive)
-        }
-    }
-    
-    // TextField to specify how many days before maintenance is due that the user would like to be notified
-    private var daysBeforeDueField: some View {
-        HStack {
-            Text("Days before due")
-            
-            Spacer()
-            
-            TextField("Days", value: $settings.daysBeforeMaintenance, format: .number.decimalSeparator(strategy: .automatic))
-                .keyboardType(.numberPad)
-                .fixedSize()
-                .textFieldStyle(.roundedBorder)
-                .focused($isInputActive)
-        }
-    }
-    
-    // Button that navigates to the Socket section of the iOS Settings app
-    private var socketSettingsButton: some View {
-        Button("Notification Settings") {
-            Task {
-                await AppSettings.openSocketSettings()
-            }
-        }
     }
 }
 
