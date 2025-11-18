eb0e7d84a4a41a898a37a4291a70510629b13882
Justin <justinrisner@gmail.com>
2025-11-18 16:06:32 -0500

    [Feature] Removed MaintenanceOnboardingView, since it is no longer used; deleted the content of OnboardingTips and replaced with Tip structs, since the old onboarding flow is no longer being used

--- SocketCD/Onboarding/OnboardingTips.swift
+++ SocketCD/Onboarding/OnboardingTips.swift
@@ -2,236 +2,73 @@
 //  OnboardingTips.swift
 //  SocketCD
 //
-//  Created by Justin Risner on 5/15/24.
+//  Created by Justin Risner on 11/17/25.
 //
 
-import SwiftUI
+import TipKit
 
-struct OnboardingTips: View {
-    @Environment(\.colorScheme) var colorScheme
-    @EnvironmentObject var settings: AppSettings
-    @Binding var showingOnboardingTip: Bool
-    let vehicle: Vehicle
-    
-    @State private var swipeActionWidth = UIScreen.main.bounds.width / 5
-    @State private var animationOffset: CGFloat = 0
-    @State private var animationDelay: Double = 0.5
-    
-    @State private var viewOpacity: Double = 0.0
-    
-    var body: some View {
-        quickFillView
+struct DashboardTip: Tip {
+    var title: Text {
+        Text("Welcome to the Dashboard")
     }
     
-    // MARK: - Views
-    
-    private var quickFillView: some View {
-        List {
-            vehicleCard
-                .accessibilityLabel("Animation, demonstrating a swipe action to add fill-up")
-            
-            tipText
-        }
-        .listStyle(.plain)
-        .background(Color.customBackground)
-        .opacity(viewOpacity)
+    var message: Text? {
+        Text("Tap the action button on a card to add or edit info, or tap the card itself to view details.")
     }
     
-    // Duplicate of first vehicle card in VehicleListView, that animates to show swiping to add a fill-up
-    private var vehicleCard: some View {
-        Group {
-            ZStack(alignment: .leading) {
-                ZStack {
-                    Rectangle()
-                        .foregroundStyle(Color.mint)
-                        .frame(width: swipeActionWidth)
-                    
-                    Image(systemName: "fuelpump.fill")
-                        .font(.title2)
-                        .foregroundStyle(.white)
-                }
-                .offset(x: animationOffset - swipeActionWidth)
-                
-                HStack(spacing: 0) {
-                    Rectangle()
-                        .foregroundStyle(Color.indigo)
-                        .frame(width: swipeActionWidth / 4)
-                    
-                    Rectangle()
-                        .foregroundStyle(Color(.red))
-                        .frame(width: swipeActionWidth / 4)
-                }
-                .offset(x: UIScreen.main.bounds.width + animationOffset)
-                
-                ZStack {
-                    Rectangle()
-                        .foregroundStyle(Color(.customBackground))
-                    
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 15)
-//                            .colorSchemeBackground(colorScheme: colorScheme)
-                            .shadow(color: .secondary.opacity(0.4), radius: colorScheme == .dark ? 0 : 2)
-                        //
-                        VStack(alignment: .leading, spacing: 0) {
-                            RoundedRectangle(cornerRadius: 10)
-                                .foregroundStyle(Color.clear)
-                                .aspectRatio(2, contentMode: .fit)
-                                .overlay(
-                                    ZStack {
-                                        if let carPhoto = vehicle.photo {
-                                            VehicleImageView(carPhoto: carPhoto)
-                                        } else {
-                                            VehicleImageView(backgroundColor: vehicle.backgroundColor)
-                                        }
-                                        
-                                        RoundedRectangle(cornerRadius: 10)
-                                            .stroke(.black.opacity(0.3), lineWidth: 0.5)
-                                            .foregroundStyle(Color.clear)
-                                    }
-                                )
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                                .padding([.top, .horizontal], 5)
-                            
-                            HStack {
-                                VStack(alignment: .leading, spacing: 0) {
-                                    Text(vehicle.name)
-                                        .font(.headline)
-                                        .lineLimit(1)
-                                    
-                                    Text("\(vehicle.odometer) \(settings.distanceUnit.abbreviated)")
-                                        .font(.caption)
-                                        .foregroundStyle(Color.secondary)
-                                }
-                                
-                                Spacer()
-                                
-                                Image(systemName: "ellipsis")
-                                    .foregroundStyle(Color.secondary)
-                            }
-                            .padding(.vertical, 7)
-                            .padding(.horizontal, 15)
-                        }
-                    }
-                    .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-                    .fixedSize(horizontal: false, vertical: true)
-                }
-                .offset(x: animationOffset)
-            }
-        }
-        .animation(.easeInOut.delay(animationDelay).speed(0.75), value: animationOffset)
-        .listRowInsets(EdgeInsets())
-        .listRowSeparator(.hidden)
-        .onAppear {
-            animateVehicleCard()
-        }
-    }
-    
-    // Explanation text, for swiping to add a fill-up
-    private var tipText: some View {
-        Section {
-            ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundStyle(Color.socketPurple)
-                    .accessibilityElement()
-                
-                VStack(alignment: .leading, spacing: 20) {
-                    Text("You can swipe on a vehicle, to quickly add fill-ups or make changes to that vehicle, right from this screen.")
-                    
-                    Text("When you're ready, tap your vehicle to begin adding maintenance services, repairs, and more.")
-                }
-                .padding(30)
-                .font(.subheadline)
-                .foregroundStyle(Color.white)
-                .accessibilityElement(children: .combine)
-            }
-            .padding(.top, 30)
-            .listRowSeparator(.hidden)
-            .listRowBackground(Color.customBackground)
-        }
-    }
-    
-    
-    // MARK: - Methods
-    
-    // Animates from quick fill tip, back to vehicle list view
-    private func closeOnboardingTip() {
-        withAnimation(.smooth) {
-            animationDelay = 0
-            animationOffset = 0
-        }
-        
-        DispatchQueue.main.async {
-            viewOpacity = 0.0
-            showingOnboardingTip = false
-        }
-    }
-    
-    // Animates the vehicle card, to give a peek at the swipe actions
-    private func animateVehicleCard() {
-        // Moving the animation to the main thread prevents unexpected animation on iOS 15
-        DispatchQueue.main.async {
-            viewOpacity = 1.0
-        }
-        
-        // iOS 15's animation behavior is slower than 16+, so the timing needed to be different to get similar effects
-        if #available(iOS 16, *) {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                withAnimation(.smooth) {
-                    animationOffset = swipeActionWidth / 2
-                }
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.25) {
-                withAnimation(.smooth) {
-                    animationOffset = -swipeActionWidth / 2
-                }
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                withAnimation(.smooth) {
-                    animationOffset = 0
-                }
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
-                withAnimation {
-                    closeOnboardingTip()
-                }
-            }
-        } else {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                withAnimation(.smooth) {
-                    animationOffset = swipeActionWidth / 2
-                }
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                withAnimation(.smooth) {
-                    animationOffset = -swipeActionWidth / 2
-                }
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                withAnimation(.smooth) {
-                    animationOffset = 0
-                }
-            }
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                withAnimation {
-                    closeOnboardingTip()
-                }
-            }
-        }
+    var image: Image? {
+        Image(systemName: "rectangle.3.group.fill")
     }
 }
 
-#Preview {
-    let context = DataController.preview.container.viewContext
-    let vehicle = Vehicle(context: context)
-    vehicle.name = "My Car"
-    vehicle.odometer = 12345
+struct MaintenanceListTip: Tip {
+    var title: Text {
+        Text("Log Maintenance")
+    }
     
-    return OnboardingTips(showingOnboardingTip: .constant(true), vehicle: vehicle)
-        .environmentObject(AppSettings())
+    var message: Text? {
+//        Text("""
+//            Tap the **book** button to set up a new maintenance service.
+//            
+//            Tap the **plus** button each time you complete an existing service to log it.
+//            """)
+        Text("Tap to set up a new maintenance service.")
+    }
+    
+    var image: Image? {
+        Image(systemName: "checklist.unchecked")
+    }
+}
+
+struct LogServiceTip: Tip {
+    var title: Text {
+        Text("Log Maintenance")
+    }
+    
+    var message: Text? {
+//        Text("Tap the **plus** button each time you complete a service to log it.")
+        Text("""
+            Tap the **book** button to set up a new maintenance service.
+            
+            Tap the **plus** button each time you complete an existing service to log it.
+            """)
+    }
+    
+    var image: Image? {
+        Image(systemName: "plus.circle.fill")
+    }
+}
+
+struct ServiceLogTip: Tip {
+    var title: Text {
+        Text("Log Multiple Services")
+    }
+    
+    var message: Text? {
+        Text("Select one or more services to log. They will share all notes, photos, and details you enter.")
+    }
+    
+    var image: Image? {
+        Image(systemName: "circle.grid.2x2.topleft.checkmark.filled")
+    }
 }
