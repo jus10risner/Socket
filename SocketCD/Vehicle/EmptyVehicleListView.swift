e75f4ed79eb15cee48285853f3f55bd2d3fa4fe7
Justin <justinrisner@gmail.com>
2025-11-19 15:42:59 -0500

    [Feature] Updated layout for iCloud sync indicator; simplified overall view layout

--- SocketCD/Vehicle/EmptyVehicleListView.swift
+++ SocketCD/Vehicle/EmptyVehicleListView.swift
@@ -11,7 +11,7 @@ struct EmptyVehicleListView: View {
     @StateObject var monitor = CloudKitSyncMonitor(container: DataController.shared.container)
     
     var body: some View {
-        ZStack {
+        ZStack(alignment: .bottom) {
             ContentUnavailableView {
                 Label {
                     Text("Add a Vehicle")
@@ -22,18 +22,14 @@ struct EmptyVehicleListView: View {
             } description: {
                 Text("Tap the plus button to get started.")
             }
-            
-            VStack {
-                Spacer()
                 
-                if monitor.isSyncing == true {
-                    HStack {
-                        ProgressView()
-                        
-                        Text("Checking for iCloud data")
-                            .font(.caption)
-                            .foregroundStyle(Color.secondary)
-                    }
+            if monitor.isSyncing == true {
+                VStack(spacing: 10) {
+                    ProgressView()
+                    
+                    Text("Checking for iCloud data")
+                        .font(.caption)
+                        .foregroundStyle(Color.secondary)
                 }
             }
         }
