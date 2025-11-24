88ea557535f5d0debdd469b0021ec80767554d70
Justin <justinrisner@gmail.com>
2025-11-24 10:06:33 -0500

    [Feature] Created a reusable pickerLabel view; simplified the picker setup, using tags instead of buttons

--- SocketCD/Settings/Subviews/AccentColorSelectorView.swift
+++ SocketCD/Settings/Subviews/AccentColorSelectorView.swift
@@ -15,9 +15,14 @@ struct AccentColorSelectorView: View {
         List {
             Section {
                 Picker("Accent Color", selection: $settings.accentColor) {
-                    defaultAccentButton
+                    pickerLabel(text: "Socket Purple", color: Color.accent)
+                        .tag(nil as AccentColors?)
                     
-                    alternateAccentButtons
+                    ForEach(AccentColors.allCases, id: \.self) { color in
+                        pickerLabel(text: color.rawValue.capitalized, color: color.value)
+                            .tag(color as AccentColors?)
+                    }
+                    
                 }
                 .pickerStyle(.inline)
                 .labelsHidden()
@@ -29,44 +34,16 @@ struct AccentColorSelectorView: View {
         .navigationTitle("Accent Color")
         .navigationBarTitleDisplayMode(.inline)
     }
-    
-    
-    // MARK: - Views
-    
-    // Button to select the default (multicolor) accent
-    private var defaultAccentButton: some View {
-        Button {
-            settings.accentColor = nil
-        } label: {
-            Label {
-                Text("Socket Purple")
-            } icon: {
-                Image(systemName: "circle.fill")
-                    .font(.title)
-                    .foregroundStyle(Color.accent)
-            }
-            .padding(5)
+
+    private func pickerLabel(text: String, color: Color) -> some View {
+        Label {
+            Text(text)
+        } icon: {
+            Image(systemName: "circle.fill")
+                .font(.title)
+                .foregroundStyle(color)
         }
-        .tag(nil as AccentColors?)
-    }
-    
-    // Buttons to select alternate accent color
-    private var alternateAccentButtons: some View {
-        ForEach(AccentColors.allCases, id: \.self) { color in
-            Button {
-                settings.accentColor = color
-            } label: {
-                Label {
-                    Text(color.rawValue.capitalized)
-                } icon: {
-                    Image(systemName: "circle.fill")
-                        .font(.title)
-                        .foregroundStyle(settings.colorValue(for: color))
-                }
-                .padding(5)
-            }
-            .tag(color as AccentColors?)
-        }
+        .padding(5)
     }
 }
 
