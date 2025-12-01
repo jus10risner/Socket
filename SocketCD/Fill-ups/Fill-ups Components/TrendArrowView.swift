0b2367409846202c2832d853aee2f868d824b4c4
Justin <justinrisner@gmail.com>
2025-12-01 13:11:01 -0500

    [Feature] Updated the trend arrow to use an overlay for the symbol, rather than a ZStack; added a single function that returns indicator symbols and accessibility labels, to simplify the code; added 'private' label to animateTrendArrow

--- SocketCD/Fill-ups/Fill-ups Components/TrendArrowView.swift
+++ SocketCD/Fill-ups/Fill-ups Components/TrendArrowView.swift
@@ -22,62 +22,49 @@ struct TrendArrowView: View {
     }
     
     var body: some View {
-        ZStack {
-            Circle()
-                .frame(width: 35)
-                .foregroundStyle(Color(.tertiarySystemGroupedBackground))
-            
-            if latestFillupFuelEconomy != 0 {
-                if latestFillupFuelEconomy > previousFillupFuelEconomy {
-                    upArrow
-                } else if latestFillupFuelEconomy < previousFillupFuelEconomy {
-                    downArrow
-                } else {
-                    equalSign
+        Circle()
+            .frame(width: 35)
+            .foregroundStyle(Color(.tertiarySystemGroupedBackground))
+            .overlay {
+                Group {
+                    if latestFillupFuelEconomy != 0 {
+                        if latestFillupFuelEconomy > previousFillupFuelEconomy {
+                            indicatorSymbol(systemName: "chevron.up", accessibilityLabel: "Fuel economy is up since your last fill-up")
+                                .offset(y: animatingTrendArrow ? 0 : 30)
+                        } else if latestFillupFuelEconomy < previousFillupFuelEconomy {
+                            indicatorSymbol(systemName: "chevron.down", accessibilityLabel: "Fuel economy is down since your last fill-up")
+                                .offset(y: animatingTrendArrow ? 0 : -35)
+                        } else {
+                            indicatorSymbol(systemName: "equal", accessibilityLabel: "Fuel economy is the same as your last fill-up")
+                        }
+                    } else {
+                        indicatorSymbol(systemName: "minus", accessibilityLabel: "Fuel economy is not available for this fill-up")
+                    }
                 }
-            } else {
-                economyUnavailableButton
+                .foregroundStyle(Color(.fillupsTheme))
+                .scaledToFit()
+                .bold()
+                .padding(8)
             }
-        }
-        .foregroundStyle(Color(.fillupsTheme))
-        .font(.title2.bold())
-        .onAppear { animateTrendArrow(shouldReset: false) }
-        .mask {
-            Circle()
-                .frame(width: 35)
-        }
-        .onChange(of: Array(fillups)) {
-            animateTrendArrow(shouldReset: true)
-        }
+            .onAppear { animateTrendArrow(shouldReset: false) }
+            .mask {
+                Circle()
+                    .frame(width: 35)
+            }
+            .onChange(of: Array(fillups)) {
+                animateTrendArrow(shouldReset: true)
+            }
     }
     
-    // Up arrow Image
-    private var upArrow: some View {
-        Image(systemName: "chevron.up")
-            .offset(y: animatingTrendArrow ? 0 : 35)
-            .accessibilityLabel("Fuel economy is up since your last fill-up")
-    }
-    
-    // Down arrow Image
-    private var downArrow: some View {
-        Image(systemName: "chevron.down")
-            .offset(y: animatingTrendArrow ? 0 : -35)
-            .accessibilityLabel("Fuel economy is down since your last fill-up")
-    }
-    
-    // Equal sign Image
-    private var equalSign: some View {
-        Image(systemName: "equal")
-            .accessibilityLabel("Fuel economy is the same as your last fill-up")
-    }
-    
-    private var economyUnavailableButton: some View {
-        Image(systemName: "minus")
-            .accessibilityLabel("Fuel economy is not available for this fill-up")
+    // Symbol to display inside the circle, along with an accessibility label to explain what the symbol means
+    private func indicatorSymbol(systemName: String, accessibilityLabel: String) -> some View {
+        Image(systemName: systemName)
+            .resizable()
+            .accessibilityLabel(accessibilityLabel)
     }
     
     // Animates trendArrow into view, with option to reset to it's original position off-screen (for animation after adding new fill-up)
-    func animateTrendArrow(shouldReset: Bool) {
+    private func animateTrendArrow(shouldReset: Bool) {
         if shouldReset == true {
             withAnimation(nil) {
                 animatingTrendArrow = false
