7a94e6f2a013905167a9112cb59212021e4f9387
Justin <justinrisner@gmail.com>
2026-04-22 11:58:38 -0400

    [Feature] Added a method that cancels all scheduled notifications, then calls refreshAllNotifications; used when alert ranges are changed in MaintenanceAlertSettingsView

--- SocketCD/Shared/Utilities/Notifications/NotificationManager.swift
+++ SocketCD/Shared/Utilities/Notifications/NotificationManager.swift
@@ -31,4 +31,39 @@ final class NotificationManager: ObservableObject {
             }
         }
     }
+    
+    // Used to update all notifications, when alert ranges are modified in app settings
+    @MainActor
+    func cancelAndRescheduleAllNotifications() async {
+        let settings = await UNUserNotificationCenter.current().notificationSettings()
+        guard settings.authorizationStatus == .authorized else {
+            print("Notifications not authorized.")
+            return
+        }
+
+        let context = DataController.shared.container.viewContext
+        let vehicles = (try? context.fetch(Vehicle.fetchRequest())) ?? []
+        let allServices: [Service] = vehicles
+            .compactMap { $0.services as? Set<Service> }
+            .flatMap { Array($0) }
+
+        // Cancel notifications for each service and clear lastScheduleNotification values
+        for service in allServices {
+            // Cancel time-based if one was scheduled
+            if service.lastScheduledNotificationDate != nil {
+                NotificationScheduler.cancelTimeBased(for: service)
+                service.lastScheduledNotificationDate = nil
+            }
+
+            // Cancel distance-based if one was scheduled
+            if service.lastScheduledNotificationOdometer != nil {
+                NotificationScheduler.cancelDistanceBased(for: service)
+                service.lastScheduledNotificationOdometer = nil
+            }
+        }
+
+        try? context.save()
+
+        await refreshAllNotifications()
+    }
 }
